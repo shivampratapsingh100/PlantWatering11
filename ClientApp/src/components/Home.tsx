63a5972bcf7570efe2672908aaedb8c32b048216
shivampratapsingh1 <66032157+shivampratapsingh1@users.noreply.github.com>
2021-09-03 08:41:35 -0400

    plant project

---
+++ ClientApp/src/components/Home.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+
+const Home = () => (
+  <div>
+    <h1>Plant Watering System</h1>
+    <p>Here you water your plants without any hasel!!</p>
+    
+    </div>
+
+
+);
+
+export default connect()(Home);
---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlantWatering.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Time to water", "Timer set to 10", "watering for 10 seconds", "good plant health", "not watered for more than 6 hours", "no need to water", "over flooded"      };
+        private static readonly string[] plants = new[]
+       {
+            "banyan tree","mango tree","peepal tree","pine tree","maple tree"     };
+
+
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                plants = plants[rng.Next(plants.Length)],
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
